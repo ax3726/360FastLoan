a70daec04ba6871369bb42c3aacb41d6bb4d079d
liming <15970060419lma>
2019-06-17 11:31:52 +0800

    测试提交代码

--- app/src/main/java/com/xjd/a360fastloan/ui/mian/MainActivity.java
+++ app/src/main/java/com/xjd/a360fastloan/ui/mian/MainActivity.java
@@ -3,13 +3,25 @@ package com.xjd.a360fastloan.ui.mian;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lm.lib_common.base.BaseActivity;
+import com.lm.lib_common.base.BasePresenter;
 import com.xjd.a360fastloan.R;
+import com.xjd.a360fastloan.databinding.ActivityLoginBinding;
+import com.xjd.a360fastloan.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity<BasePresenter, ActivityMainBinding> {
+    @Override
+    protected boolean isTitleBar() {
+        return false;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
     }
 }
