326e1c560634e7f10c7c07a67286217186b1d287
liming <15970060419lma>
2019-07-02 17:28:55 +0800

    添加选择PhotoActivity

--- app/src/main/java/com/xjd/a360fastloan/ui/home/AddIdCardActivity.java
+++ app/src/main/java/com/xjd/a360fastloan/ui/home/AddIdCardActivity.java
@@ -1,16 +1,26 @@
 package com.xjd.a360fastloan.ui.home;
 
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.lm.lib_common.base.BaseActivity;
 import com.lm.lib_common.base.BasePresenter;
+import com.lm.lib_common.utils.dialog.MyItemDialogListener;
+import com.lm.lib_common.utils.dialog.StytledDialog;
 import com.xjd.a360fastloan.R;
 import com.xjd.a360fastloan.databinding.ActivityAddIdCardBinding;
+import com.xjd.a360fastloan.ui.common.PhotoActivity;
 import com.xjd.a360fastloan.widget.dialog.BackDialog;
 
-public class AddIdCardActivity extends BaseActivity<BasePresenter, ActivityAddIdCardBinding> {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+public class AddIdCardActivity extends PhotoActivity<BasePresenter, ActivityAddIdCardBinding> {
+
+    private int mType = 0;//0 正面 1 反面
 
     @Override
     protected int getLayoutId() {
@@ -38,6 +48,7 @@ public class AddIdCardActivity extends BaseActivity<BasePresenter, ActivityAddId
                 backDialog.show();
             }
         });
+
     }
 
     @Override
@@ -47,6 +58,60 @@ public class AddIdCardActivity extends BaseActivity<BasePresenter, ActivityAddId
             @Override
             public void onClick(View v) {
                 startActivity(AddContactInfoActivity.class);
+            }
+        });
+        mBinding.flyJust.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final List<String> strings = new ArrayList<>();
+                strings.add("拍照");
+                strings.add("从相册选择");
+                StytledDialog.showBottomItemDialog(aty, strings, "安全退出", true, true, new MyItemDialogListener() {
+                    @Override
+                    public void onItemClick(String text, int position) {
+                        mType = 0;
+                        switch (position) {
+                            case 0:
+                                doPhoto();
+                                break;
+                            case 1:
+                                pickphoto();
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onBottomBtnClick() {
+                        showToast("onItemClick");
+                    }
+                });
+            }
+        });
+        mBinding.flyBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final List<String> strings = new ArrayList<>();
+                strings.add("拍照");
+                strings.add("从相册选择");
+                StytledDialog.showBottomItemDialog(aty, strings, "安全退出", true, true, new MyItemDialogListener() {
+                    @Override
+                    public void onItemClick(String text, int position) {
+                        mType = 1;
+                        switch (position) {
+                            case 0:
+                                doPhoto();
+                                break;
+                            case 1:
+                                pickphoto();
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onBottomBtnClick() {
+                        showToast("onItemClick");
+                    }
+                });
             }
         });
     }
@@ -66,4 +131,19 @@ public class AddIdCardActivity extends BaseActivity<BasePresenter, ActivityAddId
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    public void photoSuccess(String path, File file, int... queue) {
+        if (!TextUtils.isEmpty(path)) {
+            if (mType == 0) {
+                Glide.with(aty).load(file).into(mBinding.imgJust);
+            } else {
+                Glide.with(aty).load(file).into(mBinding.imgBack);
+            }
+        }
+    }
+
+    @Override
+    public void photoFaild() {
+
+    }
 }
